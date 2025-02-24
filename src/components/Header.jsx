cc9ff540e8d556ddc65099668bf3c69523f5bd00
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-02-24 09:01:42 -0800

    header color change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="bg-orange-100 text-black shadow-md">
+    <header className="bg-gray-900 text-white">
       <div className="container mx-auto px-6 py-4 flex justify-between items-center">
         {/* Logo / Brand */}
         <Link to="/" className="text-2xl font-bold">
